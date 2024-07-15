e4e97761caa4fcc1d4e94567a6112c67fccaf43f
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-15 12:00:24 +0500

    Day 2: basic arithmetic operations

---
+++ Day2/FR-1.js
@@ -0,0 +1,23 @@
+// Arithmetic Operation Script: Write a script that performs basic artihmetic operations on two numbers and logs the results
+
+let num1 = 10;
+let num2 = 20;
+
+let result;
+
+result = num1 + num2;
+console.log("Addition of two numbers is: ", result);
+
+result = num1 - num2;
+console.log("Subtraction of two numbers is: ", result);
+
+result = num1 * num2;
+console.log("Multiplication of two numbers is: ", result);
+
+result = num1 / num2;
+console.log("Division of two numbers is: ", result);
+
+result = num1 % num2;
+console.log("Remainder of two numbers is: ", result);
+
+
