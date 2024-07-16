d333cec073a861991712a8cd0f92de191cfc45f7
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-16 11:47:44 +0500

    day 4: nested loop and do-while loop task complete

--- Day4/loop.js
+++ Day4/loop.js
@@ -38,8 +38,63 @@ console.log("sum of number 1 to 10: " + sum);
 let j = 10;
 
 while ( j >= 1){
-    console.log(j); 
+    // console.log(j); 
     j--;
+}
+
+// **************************** Task 3 : do-while Loop *************************
+
+
+// 5. print number form 1 to 5 using do-while loop
+
+let k = 1;
+
+do {
+    // console.log(k);
+    k++; // increment
+} while (k <= 5);
+
+
+// 6. program to calculate the factorial of a number using do-while loop
+
+let number = 5;
+let factorial = 1;
+
+let l = 1;
+do {
+    factorial *= l;
+    l++;
+    // console.log("Factorial of a number: ", factorial);
+} while (l <= number);
+
+// *************************** Task 4 : Nested Loop ***************************
+
+// 7. print the following pattern using nested loop
+
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+for (let i = 1; i <= 5; i++){
+     let pattern = " ";
+     for (let j = 1; j <= i; j++){
+         pattern += "* ";
+     }
+    //  console.log(pattern);
+}
+
+
+// ************************* Task 5: for loop with break ************************
+
+// 8. print number from 1 to 10 using for loop but skip the loop after 5 using continue.
+
+for (let i = 1; i <= 10; i++){
+    if (i === 5) {
+        continue;
+    }
+    // console.log(i);
 }
 
 
