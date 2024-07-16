25c5dcf125758f70a23da7402f2ca8fa987893ec
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-16 11:20:55 +0500

    day 4: for loop task complete

---
+++ Day4/loop.js
@@ -0,0 +1,33 @@
+// Day 4 JS challenge - Loops
+
+// **************************** Task 1 : for Loop ****************************
+
+// 1. print numbers from 1 to 10 using for loop
+
+for (let i = 1; i <=10; i++){
+    // console.log("Number is: ", i);
+}
+
+// 2. print multiplication table of 5 using for loop
+
+let num = 5;
+
+for (let i = 1; i <=10; i++){
+    // console.log("Multiplication of 5 using for loops: ", num);
+    // console.log(num, " * ", i, " = ", num * i);
+}
+
+// *********************** Task 2 : while Loop ******************************
+
+// 3. calculate the sum of num from 1 to 10 using while loop
+
+let sum = 0;
+
+let i = 1;
+
+while (i <= 10) {
+    sum += i;
+    sum++;
+    i++;
+    console.log("Sum of numbers from 1 to 10: ", sum);
+}
