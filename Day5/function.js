98fbb1210cff871088a2bacae44e22b0cbf47cbd
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-17 15:58:52 +0500

    Day 5: function expressions

--- Day5/function.js
+++ Day5/function.js
@@ -25,3 +25,29 @@ console.log(square(2));
 // ********************************* Task 2: function expression *********************************
 
 // 3. function expression to find the maximum of two numbers and return the result.
+
+function maxOfTwoNum (a, b){
+    if (a > b){
+        return `The maximum number is ${a}`;
+    }else if(b > a){
+        return `The maximum number is ${b}`;
+    }else{
+        return 'Both numbers are equal';
+    }
+}
+
+// console.log(maxOfTwoNum(10, 20));
+// console.log(maxOfTwoNum(20, 10));
+// console.log(maxOfTwoNum(10, 10));
+
+// 4. function expression to concatenate two string and return the result.
+
+function concatString (str1, str2){
+    return str1 + " " + str2;
+}
+
+// console.log(concatString('Hello', 'World'));
+// console.log(concatString('Good', 'Morning'));
+// console.log(concatString('Abdul', 'Haseeb'));
+
+// ********************************* Task 3: Arrow function *********************************
