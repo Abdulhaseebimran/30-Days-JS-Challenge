859cdce1bd0d1886471a5fa84bd4006723d3e3bb
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-18 12:19:53 +0500

    day 6: array creations

---
+++ Day6/array.js
@@ -0,0 +1,23 @@
+// 30 days - Js challenge - Arrays
+
+// **************************** Task 1: Arrays creation and access ****************************
+
+// Task 1:
+
+const numArr = [1,2,3,4,5];
+
+console.log(numArr);
+// console.log(numArr[0]);
+// console.log(numArr["1"]);
+
+// Task 2:
+
+const firstElement = numArr[0];
+console.log("first element of array: ",firstElement);
+
+const lastElement = numArr[numArr.length-1];
+console.log("Last Element of array: ",lastElement);
+
+
+
+
