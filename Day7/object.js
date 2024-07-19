91d772dc8598b6bca980d20b364995ddf1110d39
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-19 11:21:33 +0500

    day 7: object creations

---
+++ Day7/object.js
@@ -0,0 +1,22 @@
+// Day 7 - JS Challenge - Object
+
+// ********************** Task 1: object creations **********************
+
+// Task 1: Create an object representing a book with properties like title, author, and year, and log the object to the console.
+
+const book = {
+    title: '30 Days of JavaScript',
+    author: 'Hitesh Choudhary',
+    year: 2024,
+}
+
+// console.log(book);
+// console.log(book.title);
+// console.log(book["year"]);
+
+// Task 2: Access and log the title and author properties of the book object.
+
+// console.log(`book ${JSON.stringify(book)}`);
+
+// console.log(`The book title is ${book.title} and the author is ${book.author}`);
+
