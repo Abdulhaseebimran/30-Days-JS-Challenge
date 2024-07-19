f544709a3697ad0abec2601e83a23bc185f2eaaa
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-19 11:21:55 +0500

    day 7: objects methods

--- Day7/object.js
+++ Day7/object.js
@@ -20,3 +20,23 @@ const book = {
 
 // console.log(`The book title is ${book.title} and the author is ${book.author}`);
 
+// ********************* Task 2: object methods **********************
+
+// Task 3: Add a method to the book object that returns a string with the book's title and author, and log the result of calling this method.
+
+book.getBookInfo = function () {
+    return `${this.title} by ${this.author}`;
+}
+
+// console.log(book);
+// console.log(book.getBookInfo());
+
+// Task 4: Add a method to the book object that takes a parameter (year) and updates the book's year property, then log the updated object.
+
+book.updateYear = function (year) {
+    this.year = year;
+}
+
+book.updateYear(2025);
+
+// console.log(`book ${JSON.stringify(book.year)}`);
