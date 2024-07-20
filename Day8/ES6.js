3518997e37d388472521d0315f8ac40d91f6f5d5
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-20 10:47:36 +0500

    day 8: template literals

---
+++ Day8/ES6.js
@@ -0,0 +1,21 @@
+// Day 8 - JS Challenge - ES6
+
+// ********************** Task 1: template literals **********************
+
+// task 1:
+
+const userName = "Abdul Haseeb";
+const userAge = 25;
+
+const user = `My name is ${userName} and I am ${userAge} years old.`;
+// console.log(user);
+
+// Task 2: multi-line strings
+
+const multiLineString = `Hello, I am Abdul Haseeb
+and I am BS Computer Science student
+at SMIU, Karachi. I am learning
+JavaScript and React.js, vue.js, and Node.js.`;
+
+// console.log(multiLineString);
+
