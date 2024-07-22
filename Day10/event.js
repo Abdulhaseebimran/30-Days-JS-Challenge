bf70955e3e0b796c302e7f76b0de6f618324a407
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-22 21:54:36 +0500

    day 10: keyboard events

--- Day10/event.js
+++ Day10/event.js
@@ -35,4 +35,23 @@ div.addEventListener("mouseout", () => {
   div.style.backgroundColor = "white";
 });
 
+// Task 3: Keyboard Events
+
+// 5: add a keydown event listener to an input field that log the value to the console.
+
+const input1 = document.querySelector(".input-1");
+
+input1.addEventListener("keydown", (e) => {
+  console.log(input1.value, e.key);
+});
+
+// 6: Add a keyup event listener to an input filed that displays the current value in a paragraph.
+
+const input2 = document.querySelector(".input-2");
+const displayPara = document.querySelector("#displayPara");
+
+input2.addEventListener("keyup", (e) => {
+  displayPara.innerHTML = input2.value;
+});
+
 
