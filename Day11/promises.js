7b8032b277efc075306dfe4d6d475bc03e4011f3
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-07-23 21:15:07 +0500

    day 11: understanding promises

---
+++ Day11/promises.js
@@ -0,0 +1,26 @@
+// Day 11 - Promises - JS Challenges
+
+// Task 1: understanding promises
+
+// 1: Create a promise that resolves with a message after a 2 second timeout and log the message to the console.
+
+const promises = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Promise resolved after 2 seconds");
+  }, 2000);
+});
+
+promises.then((message) => {
+  console.log(message);
+});
+
+// 2: create a promise that rejects with an error message after a 2 second timeout handle the error using `.catch()`.
+
+const promises2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject("Promise rejected after 2 seconds");
+  }, 2000);
+});
+
+promises2.then((res) => console.log(res)).catch((err) => console.log(err));
+
