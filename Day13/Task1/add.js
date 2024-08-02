7288200efb23a6debc33d7e177bc5fe71febbc00
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-08-03 00:08:16 +0500

    day 13: exporting modules

---
+++ Day13/Task1/add.js
@@ -0,0 +1,4 @@
+import addTwoNum from './task1.js';
+
+console.log(addTwoNum(2, 3));
+console.log(addTwoNum(5, 5));
---
+++ Day13/Task1/task1.js
@@ -0,0 +1,11 @@
+// Day 13 - JS Challenge - Modules
+
+// ********** Task 1: creating and exporting modules **********
+
+// Task 1:
+
+function addTwoNum (num1, num2) {
+    return num1 + num2;
+}
+
+export default addTwoNum;
