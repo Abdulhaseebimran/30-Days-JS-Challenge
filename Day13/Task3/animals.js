186b3162f855a009573ffde74d102c610f7f5aed
Abdulhaseebimran <haseebimran0001@gmail.com>
2024-08-03 00:09:34 +0500

    day 13: named and default exports in modules

---
+++ Day13/Task3/animals.js
@@ -0,0 +1,20 @@
+// Activity 2: Named and Default Exports
+
+// Task 3: Create a module that exports multiple functions using named exports. Import and use these functions in another script.
+
+
+export function Dog() {
+    console.log("Dog barks");
+}
+
+export function Cat() {
+    console.log("Cat meows");
+}
+
+export function Cow() {
+    console.log("Cow moos");
+}
+
+export function Horse() {
+    console.log("Horse neighs");
+}
---
+++ Day13/Task3/main3.js
@@ -0,0 +1,6 @@
+import {Cat,Cow,Dog,Horse} from './animals.js';
+
+console.log(Cat());
+console.log(Cow());
+console.log(Dog());
+console.log(Horse());
